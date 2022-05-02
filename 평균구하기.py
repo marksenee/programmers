40a15bd2672a017f7b95fff4ffdb313e9f676c04
marksenee <mark233891@gmail.com>
2022-05-02 11:36:59 +0900

    first commit

---
+++ 평균구하기.py
@@ -0,0 +1,11 @@
+# arr = []
+# print(sum(arr))
+# print(len(arr))
+# print(sum(arr)/len(arr))
+
+# 평균 구하기 
+# arr의 element들의 합 / element의 개수 
+
+arr = [1,2,3,4]
+def solution(arr) : 
+    print(sum(arr) / len(arr))
