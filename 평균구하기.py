7c1b49fa6df1ea19e7ce5cb245d33650253b0579
marksenee <mark233891@gmail.com>
2022-05-02 11:43:29 +0900

    test

--- 평균구하기.py
+++ 평균구하기.py
@@ -4,8 +4,8 @@
 # print(sum(arr)/len(arr))
 
 # 평균 구하기 
-# arr의 element들의 합 / element의 개수 
+# arr의 element들의 합 / element의 개수
 
 arr = [1,2,3,4]
-def solution(arr) : 
-    print(sum(arr) / len(arr))
+# def solution(arr) : 
+#     print(sum(arr) / len(arr))
